145d3be0706a9837985a730ada44398a3ff3d02e
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-03 16:35:04 -0300

    refactor: use env variables for db conn

--- populate_new/populate_new/dbconn.py
+++ populate_new/populate_new/dbconn.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from db_plugins.db.mongo._connection import MongoConnection
 from bson.raw_bson import RawBSONDocument
+import os
 
 
 # import models so that they can be created by MongoConnection
@@ -14,7 +15,6 @@ def create_indexes():
             "serverSelectionTimeoutMS": 3000,  # 3 second timeout
             "port": 27018,
             "database": "new_db",
-            "authSource": "admin",
         }
     )
     db.create_db()
@@ -22,15 +22,11 @@ def create_indexes():
 
 def create_mongo_connections():
     print("connecting to source database")
-    source_client = MongoClient(
-        "mongodb://localhost:27017/",
-        document_class=RawBSONDocument,
-    )
+    url = os.getenv("SOURCE_MONGODB_URL", "mongodb://localhost:27017/")
+    source_client = MongoClient(url, document_class=RawBSONDocument)
     print("connecting to target database")
-    target_client = MongoClient(
-        "mongodb://localhost:27018/",
-        document_class=RawBSONDocument,
-    )
+    url = os.getenv("TARGET_MONGODB_URL", "mongodb://localhost:27018/")
+    target_client = MongoClient(url, document_class=RawBSONDocument)
     source_db = source_client.old_db
     target_db = target_client.new_db
     return source_db, target_db
