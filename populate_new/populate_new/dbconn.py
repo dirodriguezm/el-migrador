96fd8f375d9fff1324f41cd04f17180674de93e3
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-03 16:48:06 -0300

    fix: hardcoded database names

--- populate_new/populate_new/dbconn.py
+++ populate_new/populate_new/dbconn.py
@@ -24,9 +24,11 @@ def create_mongo_connections():
     print("connecting to source database")
     url = os.getenv("SOURCE_MONGODB_URL", "mongodb://localhost:27017/")
     source_client = MongoClient(url, document_class=RawBSONDocument)
+    source_database = url.split("/")[-1]
+    source_db = source_client[source_database]
     print("connecting to target database")
     url = os.getenv("TARGET_MONGODB_URL", "mongodb://localhost:27018/")
     target_client = MongoClient(url, document_class=RawBSONDocument)
-    source_db = source_client.old_db
-    target_db = target_client.new_db
+    target_database = url.split("/")[-1]
+    target_db = target_client[target_database]
     return source_db, target_db
