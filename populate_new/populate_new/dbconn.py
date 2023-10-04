0f0306906f05933ca9dfa920e5f2e58b6ab974d1
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-04 09:47:49 -0300

    chore: black format

--- populate_new/populate_new/dbconn.py
+++ populate_new/populate_new/dbconn.py
@@ -19,7 +19,8 @@ def create_indexes():
     )
     db.create_db()
 
-def read_env_variables(which = "SOURCE"):
+
+def read_env_variables(which="SOURCE"):
     host = os.getenv(f"MONGODB_HOST_{which}", "localhost")
     port = int(os.getenv(f"MONGODB_PORT_{which}", 27017))
     database = os.getenv(f"MONGODB_DATABASE_{which}", "")
@@ -28,13 +29,28 @@ def read_env_variables(which = "SOURCE"):
     auth_source = os.getenv(f"MONGODB_AUTH_SOURCE_{which}", "admin")
     return host, port, database, username, password, auth_source
 
+
 def create_mongo_connections():
     print("connecting to source database")
     host, port, database, username, password, auth_source = read_env_variables("SOURCE")
-    source_client = MongoClient(host=host, port=port, username=username, password=password, authSource=auth_source, document_class=RawBSONDocument)
+    source_client = MongoClient(
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+        authSource=auth_source,
+        document_class=RawBSONDocument,
+    )
     source_db = source_client[database]
     print("connecting to target database")
     host, port, database, username, password, auth_source = read_env_variables("TARGET")
-    target_client = MongoClient(host=host, port=port, username=username, password=password, authSource=auth_source, document_class=RawBSONDocument)
+    target_client = MongoClient(
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+        authSource=auth_source,
+        document_class=RawBSONDocument,
+    )
     target_db = target_client[database]
     return source_db, target_db
