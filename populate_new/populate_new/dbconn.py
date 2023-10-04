47fffe6bd7b2428fe584b79da5efaad773863a46
Ubuntu <ubuntu@ip-30-0-63-43.ec2.internal>
2023-10-04 12:46:39 +0000

    refactor: use separated connection settings instead of url

--- populate_new/populate_new/dbconn.py
+++ populate_new/populate_new/dbconn.py
@@ -19,15 +19,22 @@ def create_indexes():
     )
     db.create_db()
 
+def read_env_variables(which = "SOURCE"):
+    host = os.getenv(f"MONGODB_HOST_{which}", "localhost")
+    port = int(os.getenv(f"MONGODB_PORT_{which}", 27017))
+    database = os.getenv(f"MONGODB_DATABASE_{which}", "")
+    username = os.getenv(f"MONGODB_USERNAME_{which}", "")
+    password = os.getenv(f"MONGODB_PASSWORD_{which}", "")
+    auth_source = os.getenv(f"MONGODB_AUTH_SOURCE_{which}", "admin")
+    return host, port, database, username, password, auth_source
 
 def create_mongo_connections():
     print("connecting to source database")
-    url = os.getenv("SOURCE_MONGODB_URL", "mongodb://localhost:27017/")
-    source_client = MongoClient(url, document_class=RawBSONDocument)
-    source_database = url.split("/")[-1]
-    source_db = source_client[source_database]
+    host, port, database, username, password, auth_source = read_env_variables("SOURCE")
+    source_client = MongoClient(host=host, port=port, username=username, password=password, authSource=auth_source, document_class=RawBSONDocument)
+    source_db = source_client[database]
     print("connecting to target database")
-    url = os.getenv("TARGET_MONGODB_URL", "mongodb://localhost:27018/")
-    target_client = MongoClient(url, document_class=RawBSONDocument)
-    target_db = target_client["alerts"]
+    host, port, database, username, password, auth_source = read_env_variables("TARGET")
+    target_client = MongoClient(host=host, port=port, username=username, password=password, authSource=auth_source, document_class=RawBSONDocument)
+    target_db = target_client[database]
     return source_db, target_db
